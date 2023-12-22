72fff736dfdc1ac1cd49f03c07df344d6e5180d6
M Price <zaptross@outlook.com>
2023-12-23 09:15:48 +1000

    chore(day 2): internalise functions

--- src/day_2/functions.rs
+++ src/day_2/functions.rs
@@ -1,22 +1,22 @@
-pub struct GameData {
+pub(crate) struct GameData {
     pub id: i32,
     pub turns: Vec<CubeSet>,
 }
 
 impl GameData {
-    pub fn new(id: i32, turns: Vec<CubeSet>) -> GameData {
+    pub(crate) fn new(id: i32, turns: Vec<CubeSet>) -> GameData {
         GameData { id, turns }
     }
 }
 
-pub struct CubeSet {
+pub(crate) struct CubeSet {
     pub red: i32,
     pub green: i32,
     pub blue: i32,
 }
 
 impl CubeSet {
-    pub fn new(red: i32, green: i32, blue: i32) -> CubeSet {
+    pub(crate) fn new(red: i32, green: i32, blue: i32) -> CubeSet {
         CubeSet { red, blue, green }
     }
 }
@@ -25,7 +25,7 @@ pub fn validate_set_of_games(set: CubeSet, games: Vec<&str>) -> i32 {
     return 0;
 }
 
-pub fn extract_game_data(game: &str) -> GameData {
+pub(crate) fn extract_game_data(game: &str) -> GameData {
     let split_game_id_and_games: Vec<&str> = game.split(": ").collect();
     let game_id: i32 = split_game_id_and_games[0]
         .split(" ")
@@ -42,7 +42,7 @@ pub fn extract_game_data(game: &str) -> GameData {
     return GameData::new(game_id, turns);
 }
 
-pub fn extract_cubes_from_turn(turn: &str) -> CubeSet {
+pub(crate) fn extract_cubes_from_turn(turn: &str) -> CubeSet {
     let mut red = 0;
     let mut green = 0;
     let mut blue = 0;
