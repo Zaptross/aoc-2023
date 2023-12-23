ddc3f978298fd06af679d29ce907768dcf2a9e66
M Price <zaptross@outlook.com>
2023-12-23 10:02:58 +1000

    refactor(day 2): rename part_one_test_set -> day_two_test_set

--- src/day_2/data.rs
+++ src/day_2/data.rs
@@ -1,6 +1,6 @@
 use super::functions::CubeSet;
 
-pub fn part_one_test_set() -> Vec<&'static str> {
+pub fn day_two_test_set() -> Vec<&'static str> {
     return vec![
         "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
         "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
