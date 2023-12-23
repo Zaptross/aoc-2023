3887e1a07b2fa638e185bfe4ef698f3f0ef3021b
M Price <zaptross@outlook.com>
2023-12-23 14:46:02 +1000

    feat(day 4): extract_vec_numbers

--- src/day_4/functions.rs
+++ src/day_4/functions.rs
@@ -11,5 +11,9 @@ pub(crate) fn evaluate_card(card: &str) -> i32 {
 }
 
 pub(crate) fn extract_vec_numbers(numbers: &str) -> Vec<i32> {
-    return vec![];
+    return numbers
+        .split(" ")
+        .filter(|x| x.len() > 0)
+        .map(|x| -> i32 { x.parse().unwrap() })
+        .collect();
 }
