1f4a0e3eff1079f62ac8320a69f9d4926c317b73
M Price <zaptross@outlook.com>
2023-12-23 14:47:23 +1000

    feat(day 4): sum_all_points

--- src/day_4/functions.rs
+++ src/day_4/functions.rs
@@ -3,7 +3,7 @@ pub fn count_points_of_scratchcards(cards: Vec<&str>) -> i32 {
 }
 
 pub(crate) fn sum_all_points(points: Vec<i32>) -> i32 {
-    return 0;
+    return points.iter().fold(0, |acc, x| acc + x);
 }
 
 pub(crate) fn evaluate_card(card: &str) -> i32 {
