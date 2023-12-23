77ca87c25857a434771b646f90f058841620b7d8
M Price <zaptross@outlook.com>
2023-12-23 11:44:34 +1000

    feat(day 3): find_all_symbols and refactor
    Changing from (x,y) model to (row,column) model for ease of use

--- src/day_3/functions.rs
+++ src/day_3/functions.rs
@@ -1,3 +1,8 @@
+// Coords are (row, column)
+// Eg: (1,2) -> 6
+// 123
+// 456
+// 789
 pub(crate) type Coord = (usize, usize);
 
 pub fn calculate_sum_of_part_numbers(schematic: Vec<&str>) -> i32 {
@@ -9,14 +14,23 @@ pub(crate) fn retrieve_adjacent_part_numbers(schematic: Vec<Vec<char>>) -> Vec<i
 }
 
 pub(crate) fn find_all_symbols(schematic: Vec<Vec<char>>) -> Vec<Coord> {
-    return vec![(0, 0)];
+    let mut symbol_coords: Vec<Coord> = Vec::new();
+
+    for row in 0..schematic.len() {
+        for column in 0..schematic[row].len() {
+            if is_symbol(schematic[row][column]) {
+                symbol_coords.append(&mut vec![(row, column)])
+            }
+        }
+    }
+
+    return symbol_coords;
 }
 
-// Such that jv[3][2] = '6'
-// from:
-//   "123"
-//   "456"
-//   "789"
-pub(crate) fn create_jagged_vec_from_vec_str(input: Vec<&str>) -> Vec<Vec<char>> {
-    return vec![vec!['0']];
+fn is_symbol(c: char) -> bool {
+    return c != '.' && !c.is_ascii_digit();
+}
+
+pub(crate) fn to_vec_char(input: Vec<&str>) -> Vec<Vec<char>> {
+    return input.iter().map(|s| s.chars().collect()).collect();
 }
--- src/day_3/tests.rs
+++ src/day_3/tests.rs
@@ -1,8 +1,8 @@
 use super::{
     data::day_three_test_set,
     functions::{
-        calculate_sum_of_part_numbers, create_jagged_vec_from_vec_str, find_all_symbols,
-        retrieve_adjacent_part_numbers, Coord,
+        calculate_sum_of_part_numbers, find_all_symbols, retrieve_adjacent_part_numbers,
+        to_vec_char, Coord,
     },
 };
 
@@ -12,38 +12,24 @@ fn day_3_calculate_sum_of_part_numbers() {
 }
 
 #[test]
-fn day_3_create_jagged_vec_from_vec_str() {
-    let input = vec!["123", "456", "789"];
-
-    let result = create_jagged_vec_from_vec_str(input);
-
-    assert_eq!(result[0][0], '1');
-    assert_eq!(result[2][1], '6');
-    assert_eq!(result[2][2], '9');
-}
-
-#[test]
 fn day_3_find_all_symbols() {
-    let cases: Vec<(Vec<Vec<char>>, Vec<Coord>)> = vec![(
-        create_jagged_vec_from_vec_str(vec!["*.#", ".+.", "+.$"]),
-        vec![(0, 0), (2, 0), (1, 1), (2, 0), (2, 2)],
+    let cases = vec![(
+        to_vec_char(vec!["*.#", ".+.", "+.$"]),
+        vec![(0, 0), (0, 2), (1, 1), (2, 0), (2, 2)],
     )];
 
     for (input, expected) in cases {
         let result = find_all_symbols(input);
 
         for coord in expected {
-            assert!(!result.contains(&coord));
+            assert!(result.contains(&coord));
         }
     }
 }
 
 #[test]
 fn day_3_retrieve_adjacent_part_numbers() {
-    let cases = vec![(
-        create_jagged_vec_from_vec_str(vec!["1*.", "...", ".*.", "111"]),
-        vec![1, 111],
-    )];
+    let cases = vec![(vec!["1*.", "...", ".*.", "111"], vec![1, 111])];
 
     for (input, expected) in cases {
         let result = retrieve_adjacent_part_numbers(input);
